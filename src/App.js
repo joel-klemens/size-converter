59df13f6c4ce2078b554cb78c22edb5b9b48cfde
Joel Klemens <joel.klemens@gmail.com>
2022-01-31 10:10:00 -0800

    Frist Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./styles/App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-Title">How big is stuff?</h1>
+            </header>
+            <div className="main-section">
+                <div className="left-side-section">
+                    <Dropdown>
+                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                            Dropdown Button
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">
+                                Action
+                            </Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">
+                                Another action
+                            </Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">
+                                Something else
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div className="right-side-section"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
